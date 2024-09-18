7c2c3f2cfb28bd6a8aa6660d0ac7571263f15ec7
Emil Berglund <emil.berglund@live.no>
2024-09-18 11:28:38 +0200

    Opprettet maven-prosjekt

---
+++ forelesning/test/src/main/java/emilbe/InformationChecker.java
@@ -0,0 +1,16 @@
+package emilbe;
+
+public class InformationChecker {
+    public int controlAge(int age) {
+        if (age < 0) {
+            return 0;
+        } 
+        else if (age > 120) {
+            return 120;
+        } 
+        else {
+            return age;
+        }
+    }
+    
+}
