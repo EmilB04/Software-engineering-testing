9400a8c4ea94d42de60fb6e52b077739f6e0bd5b
Emil Berglund <emil.berglund@live.no>
2024-09-27 18:13:45 +0200

    Add varsel-app

---
+++ App_utvikling/varsel-app/src/components/MainButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-btn
+    :label="props.label"
+    :to="props.to"
+    class="q-pa-md"
+    color="primary"
+    text-color="black"
+    style="border-radius: 15px;"
+  />
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+    label: string
+    to: string
+  }>()
+</script>
