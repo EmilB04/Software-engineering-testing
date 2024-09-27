a82372b1fafda562079784d886c662bf412fd30d
Emil Berglund <emil.berglund@live.no>
2024-09-27 20:29:31 +0200

    Add varsel-app

---
+++ App_utvikling/varsel-app/src/pages/auth/GettingStarted.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page>
+    Get Started Page
+  </q-page>
+</template>
+
+<script setup lang="ts">
+console.log('Get started Page')
+</script>
---
+++ App_utvikling/varsel-app/src/pages/auth/RegisterPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page>
+    Register Page
+  </q-page>
+</template>
+
+<script setup lang="ts">
+console.log('Register Page')
+</script>
