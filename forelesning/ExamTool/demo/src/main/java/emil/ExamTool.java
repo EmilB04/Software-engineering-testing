31335f01382f4dfdc0e70dfea83f35cce1d72fa2
Emil Berglund <emil.berglund@live.no>
2024-10-02 10:47:01 +0200

    First commit - Fungerende test

---
+++ forelesning/ExamTool/demo/src/main/java/emil/ExamTool.java
@@ -0,0 +1,8 @@
+package emil;
+import java.util.ArrayList;
+
+public class ExamTool {
+    public int countGradeInList(String a, ArrayList<String> grades){
+        return 3;
+    }
+}
---
+++ forelesning/ExamTool/demo/src/test/java/ExamToolTests.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+import emil.ExamTool;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import java.util.Collections;
+
+public class ExamToolTests {
+    @Test
+    @DisplayName("Count grade i list of grades")
+
+    public void countGradeInList() {
+        ExamTool examTool = new ExamTool();
+
+        ArrayList<String> grades = new ArrayList<>();
+        Collections.addAll(grades, "A", "B", "C", "D", "A", "A", "B", "C", "D", "F");
+
+        int countA = examTool.countGradeInList("A", grades);
+        
+        Assertions.assertEquals(3, countA);
+    }
+}
