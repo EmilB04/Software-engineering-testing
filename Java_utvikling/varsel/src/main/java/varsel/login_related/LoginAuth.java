172bc896fa193db6455198f67ecef608b8756194
EmilB04 <emil.berglund@live.no>
2024-10-03 21:15:57 +0200

    Class for login autentication

--- Java_utvikling/varsel/src/main/java/varsel/login_related/LoginAuth.java
+++ Java_utvikling/varsel/src/main/java/varsel/login_related/LoginAuth.java
@@ -1,5 +1,20 @@
 package varsel.login_related;
 
+// Path: varsel/src/main/java/varsel/login_related/LoginAuth.java
+// Use-case: LoginAuth.java is a class that provides methods to authenticate a user.
+
 public class LoginAuth {
-    
+    private UserRepo userRepo;
+
+    public LoginAuth(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public boolean login(double user_id, String password) { // Method to authenticate a user
+        User user = userRepo.findUserById(user_id); 
+        if (user != null && user.getPassword().equals(password)) { 
+            return true;
+        }
+        return false;
+    }
 }
