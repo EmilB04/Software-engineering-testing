5a2a7647d10629d0b2f57df063bdfbcdaa4d337b
EmilB04 <emil.berglund@live.no>
2024-10-03 20:51:35 +0200

    Made relevant classes and test-file

---
+++ Java_utvikling/varsel/src/main/java/varsel/LoginAuth.java
@@ -0,0 +1,5 @@
+package varsel;
+
+public class LoginAuth {
+    
+}
---
+++ Java_utvikling/varsel/src/test/java/LoginAuthTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LoginAuthTests {
+    @Test
+    @DisplayName("Test login with valid credentials")
+    void testLoginWithValidCredentials() {
+        
+    }
+
+
+
+}
