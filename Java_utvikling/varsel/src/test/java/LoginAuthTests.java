32a55990713c4368a2469d088c1a141960598127
EmilB04 <emil.berglund@live.no>
2024-10-03 21:52:15 +0200

    Working test for invalid login

--- Java_utvikling/varsel/src/test/java/LoginAuthTests.java
+++ Java_utvikling/varsel/src/test/java/LoginAuthTests.java
@@ -28,4 +28,21 @@ public class LoginAuthTests {
         assertTrue(loginSuccess, "Login should be successful");                         // Forventer at loginSuccess er true
 
     }
+
+    @Test
+    @DisplayName("Test login with invalid credentials")
+    void testLoginFail() {
+        // Arrange
+        UserRepo userRepo = Mockito.mock(UserRepo.class);                           // Simulere en database med brukere
+        User testUser1 = new User(1, "testuser@hiof.no", "password");    // Oppretter en testbruker
+
+        when(userRepo.findUserById(1)).thenReturn(testUser1);                           // Når findUserById(1) kalles, returner testUser1
+
+        // Act
+        LoginAuth loginAuth = new LoginAuth(userRepo);                                          // Bruker UserRepo for å autentisere brukere i LoginAuth klassen
+        boolean loginFail = loginAuth.login(1, "wrongpassword");          // Prøver å logge inn med feil passord
+
+        // Assert
+        assertFalse(loginFail, "Login should fail");                                      // Forventer at loginSuccess er false
+    }
 }
