942a19191d886090f05c446dd69fe872ccb2ffec
EmilB04 <emil.berglund@live.no>
2024-10-03 21:03:29 +0200

    Commented file

--- Java_utvikling/varsel/src/main/java/varsel/login_related/UserRepo.java
+++ Java_utvikling/varsel/src/main/java/varsel/login_related/UserRepo.java
@@ -1,5 +1,9 @@
 package varsel.login_related;
 
+// Path: varsel/src/main/java/varsel/login_related/UserRepo.java
+// Use-case: UserRepo.java is an interface that provides methods to interact with the database.
+// Methods: findUserById(double user)
+// Dependencies: User.java
 
 public interface UserRepo {
     User findUserById(double user_id);
