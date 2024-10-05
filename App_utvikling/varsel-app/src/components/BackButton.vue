58d0904cdcb6d7a3e49e5934957acb4a92c27d08
Emil Berglund <emil.berglund@live.no>
2024-10-05 16:08:19 +0200

    Finished back button prop

--- App_utvikling/varsel-app/src/components/BackButton.vue
+++ App_utvikling/varsel-app/src/components/BackButton.vue
@@ -2,8 +2,7 @@
   <img
     src="/public/c_icons/backButton.svg"
     :to="props.to"
-    style="width: 55px; height: 55px;"
-    class="q-mt-xl"
+    style="width: 55px; height: 55px; margin: 69px 0px 0px 44px;"
   />
 </template>
 
