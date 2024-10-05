49d033f88197d15336cb16c22d5f11a7fb194c4a
Emil Berglund <emil.berglund@live.no>
2024-10-05 16:08:05 +0200

    Made prop for title-text

---
+++ App_utvikling/varsel-app/src/components/auth/TitleText.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1
+    :label="props.label"
+    class="text-h4 text-weight-medium"
+  />
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+    label: string
+    to: string
+  }>()
+</script>
