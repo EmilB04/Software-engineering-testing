7bccc6fbe9d565a55b96867111547c370db0225b
Emil Berglund <emil.berglund@live.no>
2024-10-05 16:16:44 +0200

    Finished title text prop

--- App_utvikling/varsel-app/src/components/auth/TitleText.vue
+++ App_utvikling/varsel-app/src/components/auth/TitleText.vue
@@ -1,14 +1,16 @@
 <template>
   <h1
     :label="props.label"
-    class="text-h4 text-weight-medium"
-  />
+    class="text-h4 text-weight-medium q-mt-sm"
+    style="color: black;"
+  >
+    {{ props.label }}
+  </h1>
 </template>
 
 <script setup lang="ts">
 
 const props = defineProps<{
     label: string
-    to: string
   }>()
 </script>
