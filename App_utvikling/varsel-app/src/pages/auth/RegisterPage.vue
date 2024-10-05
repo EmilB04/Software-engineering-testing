158d6165f70bd006ac52fc565a0029bde1263ae5
Emil Berglund <emil.berglund@live.no>
2024-10-05 16:16:55 +0200

    Corrected register page with title text

--- App_utvikling/varsel-app/src/pages/auth/RegisterPage.vue
+++ App_utvikling/varsel-app/src/pages/auth/RegisterPage.vue
@@ -4,13 +4,14 @@
   >
     <BackButton to="/auth/welcome"/>
     <div class="column items-center text-center q-mt-lg col">
-      <h1 class="text-h4 text-weight-medium">Registrer deg</h1>
+      <TitleText label="Registrer deg"/>
     </div>
     Register Page
   </q-page>
 </template>
 
 <script setup lang="ts">
+import TitleText from 'src/components/auth/TitleText.vue'
 import BackButton from 'src/components/BackButton.vue'
 
 console.log('Register Page')
