dfdbd3ab4ea3aa801ddad2a76b197a1f8f5485e3
Emil Berglund <emil.berglund@live.no>
2024-10-05 17:09:31 +0200

    Designed login page partially

--- App_utvikling/varsel-app/src/pages/auth/LoginPage.vue
+++ App_utvikling/varsel-app/src/pages/auth/LoginPage.vue
@@ -1,13 +1,45 @@
 <template>
   <q-page
-    class="column items-center c-page"
-    style="width: clamp(320px, 50vw, 380px);"
+    class="column q-pa-none bg-grey-2"
   >
-    <q-space />
-    <div class="full-width flex justify-center">
-      logo
+    <BackButton to="/auth/welcome"/>
+    <div class="column items-center text-center q-mt-lg col">
+      <TitleText label="Velkommen tilbake!<br>Logg inn"/>
+
+      <div style="display: flex; flex-direction: column; gap: 1vh; width: 270px;;">
+        <q-input
+          v-model="email"
+          label="E-post"
+          outlined
+          class="q-mt-xl"
+          style="width: 100%;"
+        />
+        <q-input
+          v-model="password"
+          label="Passord"
+          type="password"
+          outlined
+          class="q-mt-md"
+          style="width: 100%;"
+        />
+        <q-btn
+          label="Glemt passord?"
+          flat
+          no-caps
+          class="q-mt-md q-pa-none text-body1 text-weight-medium"
+          style="text-decoration: underline; align-self: flex-start;"
+        />
+      </div>
+
     </div>
-    <q-space />
+    <div class="column items-center text-center">
+      <MainButton
+        label="Logg inn"
+        to="/"
+        class="q-mt-md"
+      />
+    </div>
+
     <div>
       <LoginBrandButton
         brand="google"
@@ -25,9 +57,14 @@ import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 import LoginBrandButton from 'components/auth/LoginBrandButton.vue'
+import TitleText from 'components/auth/TitleText.vue'
+import MainButton from 'components/MainButton.vue'
+import BackButton from 'src/components/BackButton.vue'
 
 const router = useRouter()
 const busy = ref(false)
+const email = ref('')
+const password = ref('')
 
 const onSignInWithGoogle = () => {
   busy.value = true
