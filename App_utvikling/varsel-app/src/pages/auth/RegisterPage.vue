036b16c9caef5b54dbdf365cc2a30bedde461c8e
Emil Berglund <emil.berglund@live.no>
2024-10-05 16:00:30 +0200

    Started on register page

--- App_utvikling/varsel-app/src/pages/auth/RegisterPage.vue
+++ App_utvikling/varsel-app/src/pages/auth/RegisterPage.vue
@@ -1,9 +1,18 @@
 <template>
-  <q-page>
+  <q-page
+    class="column items-center q-pa-none bg-grey-2"
+  >
+    <div class="column items-center text-center q-mt-lg col">
+      <!-- override this picture to be on the left-->
+      <BackButton to="/auth/welcome" />
+      <h1 class="text-h4 text-weight-medium">Registrer deg</h1>
+    </div>
     Register Page
   </q-page>
 </template>
 
 <script setup lang="ts">
+import BackButton from 'src/components/BackButton.vue'
+
 console.log('Register Page')
 </script>
