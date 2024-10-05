7d09b84b05a64b0211e77ad83bc50a4097693601
Emil Berglund <emil.berglund@live.no>
2024-10-05 15:58:47 +0200

    Made prop for back-button

---
+++ App_utvikling/varsel-app/src/components/BackButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    src="public/c_icons/Keyboard arrow left.svg"
+    :to="props.to"
+    style="width: 55px; height: 55px;"
+    class="q-mt-xl"
+  />
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+    to: string
+  }>()
+</script>
