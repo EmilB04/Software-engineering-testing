e768a6f58ff2764881b926be9efd1ce704cd7efe
Emil Berglund <emil.berglund@live.no>
2024-10-07 13:06:14 +0200

    Changed path to file at request from vue

--- App_utvikling/varsel-app/src/components/BackButton.vue
+++ App_utvikling/varsel-app/src/components/BackButton.vue
@@ -1,6 +1,6 @@
 <template>
   <img
-    src="/public/c_icons/backButton.svg"
+    src="/c_icons/backButton.svg"
     :to="props.to"
     style="width: 55px; height: 55px; margin: 69px 0px 0px 44px;"
   />
