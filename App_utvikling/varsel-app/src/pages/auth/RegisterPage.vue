e4cf6bda26e0dc3c1a16eafb74cc807501b9ceda
Emil Berglund <emil.berglund@live.no>
2024-10-14 17:13:19 +0200

    Added google logo for sign-in

--- App_utvikling/varsel-app/src/pages/auth/RegisterPage.vue
+++ App_utvikling/varsel-app/src/pages/auth/RegisterPage.vue
@@ -48,22 +48,49 @@
       />
     </div>
     <div class="q-mt-xl column items-center text-center">
-      <p>Eller fortsett med:</p>
-      <!-- Google innlogging-->
+      <p style="margin: 0;">Eller fortsett med:</p>
+      <LoginGoogleButton
+        @click="onSignInWithGoogle()"
+        :loading="busy"
+        style="margin: 0; padding: 0;"
+      />
     </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
-// import LoginGoogleButton from 'src/components/auth/LoginGoogleButton.vue'
-import TitleText from 'src/components/auth/TitleText.vue'
+// import LoginBrandButton from 'components/auth/LoginBrandButton.vue'
+import LoginGoogleButton from 'components/auth/LoginGoogleButton.vue'
+import TitleText from 'components/auth/TitleText.vue'
+import MainButton from 'components/MainButton.vue'
 import BackButton from 'src/components/BackButton.vue'
-import MainButton from 'src/components/MainButton.vue'
 
 const name = ref('')
 const email = ref('')
 const password = ref('')
+const busy = ref(false)
+const router = useRouter()
+
+const onSignInWithGoogle = () => {
+  busy.value = true
+  const provider = new GoogleAuthProvider()
+  signInWithPopup(getAuth(), provider)
+    .then((result: any) => {
+      if (result._tokenResponse.isNewUser) {
+        router.push('/auth/register')
+      }
+      else {
+        router.push('/')
+      }
+    })
+    .catch((error) => {
+      console.warn('Error: ', error)
+      busy.value = false
+    })
+}
 
 </script>
