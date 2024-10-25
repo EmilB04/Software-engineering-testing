9eb33bc3bb577416f12db55618e2d648fdd87f13
idatol <ida.k.tollaksen@hiof.no>
2024-10-25 21:22:48 +0200

    Opprettet kode for test notifikasjoner

--- varsling/src/main/java/com/varsling/Main.java
+++ varsling/src/main/java/com/varsling/Main.java
@@ -1,7 +1,18 @@
 package com.varsling;
 
+import com.varsling.modules.FirebaseInitializer;
+import com.varsling.modules.Notifikasjon;
+import com.varsling.modules.PushNotifikasjonSender;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Initialiser Firebase
+        FirebaseInitializer.initializeFirebaseApp();
+
+        // Opprett en notifikasjon
+        Notifikasjon notifikasjon = new Notifikasjon("YOUR_DEVICE_FCM_TOKEN", "Test Notifikasjon", "Dette er en test.");
+
+        // Send notifikasjonen
+        PushNotifikasjonSender.sendNotifikasjon(notifikasjon);
     }
 }
--- varsling/src/main/java/com/varsling/modules/PushNotifikasjonSender.java
+++ varsling/src/main/java/com/varsling/modules/PushNotifikasjonSender.java
@@ -4,27 +4,32 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 
-
 public class PushNotifikasjonSender {
 
-    public static void PushNotifikasjonSender(String token, String title, String body){
+    /**
+     * Metode for å sende en notifikasjon til en spesifikk enhet ved bruk av et Notifikasjon-objekt.
+     * Metoden tar inn et Notifikasjon-objekt som inneholder enhetens token, tittel og tekst.
+     * Hvis det oppstår en feil under sending av notifikasjonen vil det skrives ut en feilmelding.
+     */
+    public static void sendNotifikasjon(Notifikasjon notifikasjon) {
         Notification notification = Notification.builder()
-                .setTitle(title)
-                .setBody(body)
+                .setTitle(notifikasjon.getTitle())
+                .setBody(notifikasjon.getBody())
                 .build();
 
         Message message = Message.builder()
-                .setToken(token)
+                .setToken(notifikasjon.getToken())
                 .setNotification(notification)
                 .build();
 
         try {
-            String response = FirebaseMessaging.getInstance().send(message);
-            System.out.println("Successfully sent message: " + response);
-        } catch (Exception e) {
+            FirebaseMessaging.getInstance().send(message);
+            //Utskriften er teknisk sett ikke nødvendig, kan fjernes etter testing
+            System.out.println("Notifikasjonen ble sendt");
+        } 
+        catch (Exception e) {
+            System.out.println("Det oppstod en feil under sending av notifikasjonen");
             e.printStackTrace();
-            System.out.println("Failed to send message!");
         }
     }
-
 }
