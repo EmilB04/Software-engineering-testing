432fa5b90ebda6ea2176894b8482fc52b1173a28
idatol <ida.k.tollaksen@hiof.no>
2024-10-27 15:58:18 +0100

    Fikset et par småproblemer og opprettet tester for notifikasjoner og pushnotifikasjeonersender

--- varsling/src/main/java/com/varsling/modules/FirebaseInitializer.java
+++ varsling/src/main/java/com/varsling/modules/FirebaseInitializer.java
@@ -14,14 +14,13 @@ public class FirebaseInitializer {
     /**
      * Bash kommando i terminalen:
      * set GOOGLE_APPLICATION_CREDENTIALS=C:\path\to\your\service-account-file.json
-     * ex:
+     * eks:
      * set GOOGLE_APPLICATION_CREDENTIALS=C:\Users\idakt\Documents\Testing\service-account-file.json
      * 
      * Metode for å initialisere Firebase.
      * Metoden sjekker om Firebase allerede er initialisert for å unngå reinitialisering.
      * Hvis Firebase ikke er initialisert, vil metoden initialisere Firebase med 
-     * lokalvariabel "GOOGLE_APPLICATION_CREDENTIALS" for å ungå sensitiv informasjon
-     * i de offentlig ligjengelige filene.
+     * lokalvariabel "GOOGLE_APPLICATION_CREDENTIALS" for å ungå å lagre sensitiv informasjon i koden.
      * Hvis det oppstår en IOException eller IllegalStateException vil det skrives ut en feilmelding.
      * Hvis Firebase alerede er initialisert gjøres ingenting. 
      */
