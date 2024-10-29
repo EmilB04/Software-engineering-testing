724c469a5388f659b21ab219f8931b244375ed5a
Emil Berglund <emil.berglund@live.no>
2024-10-29 21:45:57 +0100

    Change img-source-type

--- App_utvikling/varsel-app/src/components/BackButton.vue
+++ App_utvikling/varsel-app/src/components/BackButton.vue
@@ -1,13 +1,14 @@
 <template>
   <router-link :to="props.to" style="display: inline-block; width: 55px; height: 55px; margin: 69px 0px 0px 44px;">
     <img
-      src="/src/assets/c_icons/backButton.svg"
+      :src="backButtonImg"
       style="width: 100%; height: 100%;"
     />
   </router-link>
 </template>
 
 <script setup lang="ts">
+import backButtonImg from 'assets/c_icons/backButton.svg'
 import { RouterLink } from 'vue-router'
 
 const props = defineProps<{
