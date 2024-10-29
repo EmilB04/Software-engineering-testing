d7bfdf61ee581a0b1b286fa02af28d14f420ea03
Emil Berglund <emil.berglund@live.no>
2024-10-29 23:55:17 +0100

    Update error-page

--- App_utvikling/varsel-app/src/pages/ErrorNotFound.vue
+++ App_utvikling/varsel-app/src/pages/ErrorNotFound.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-black text-white text-center q-pa-md flex flex-center">
     <div>
       <div style="font-size: 30vh">
         404
       </div>
 
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+      <div
+        class="text-h2"
+        style="opacity:.4"
+      >
+        Oops. Her var det visst tomt.
       </div>
 
       <q-btn
         class="q-mt-xl"
         color="white"
-        text-color="blue"
+        text-color="primary"
         unelevated
         to="/"
-        label="Go Home"
+        label="Gå tilbake"
         no-caps
       />
     </div>
