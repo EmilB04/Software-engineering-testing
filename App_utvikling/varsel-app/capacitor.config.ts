78ba299926b5ae52cad9aa151f3129105476f781
Emil Berglund <emil.berglund@live.no>
2024-10-29 20:52:44 +0100

    Capacitator fix

--- App_utvikling/varsel-app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.varsel.app',
-  appName: 'varsel-app',
-  webDir: 'www'
-};
-
-export default config;
