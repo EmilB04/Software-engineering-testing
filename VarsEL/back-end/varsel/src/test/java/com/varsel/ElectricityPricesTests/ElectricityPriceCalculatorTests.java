c05e29eb340a190cab0b46bc4f8bd566007d9d1b
Andolaus <andolaus@gmail.com>
2024-11-09 10:41:00 +0100

    Startet å teste priceCalculator, men plutselig fungerer ikke flere av pakkene til klassene mine

--- VarsEL/back-end/varsel/src/test/java/com/varsel/ElectricityPricesTests/ElectricityPriceCalculatorTests.java
+++ VarsEL/back-end/varsel/src/test/java/com/varsel/ElectricityPricesTests/ElectricityPriceCalculatorTests.java
@@ -1,5 +1,25 @@
 package com.varsel.ElectricityPricesTests;
 
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ElectricityPrices.ElectricityPriceCalculator;
+import ElectricityPrices.ElectricityPriceData;
+
 public class ElectricityPriceCalculatorTests {
     
+    @Test
+    @DisplayName("Calculate average price - See if empty list throws exception")
+    void calculateAveragePriceEmptyListThrowsException () {
+        // Arrange
+        ElectricityPriceCalculator calculator = new ElectricityPriceCalculator();
+        List<ElectricityPriceData> prices = Collections.emptyList();
+
+        // Act and Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculator.calculateAveragePrice(prices);
+        });
+    }
 }
