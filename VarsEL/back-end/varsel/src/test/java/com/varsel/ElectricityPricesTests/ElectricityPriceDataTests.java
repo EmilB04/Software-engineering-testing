ed370907d5e7b8ccf2e4b7e116e5488cd9ed8434
Andolaus <andolaus@gmail.com>
2024-11-12 14:24:19 +0100

    Testet ElectricityPriceData med vanlige verdier, negative prisverdier og hvis tidsparametre er null

--- VarsEL/back-end/varsel/src/test/java/com/varsel/ElectricityPricesTests/ElectricityPriceDataTests.java
+++ VarsEL/back-end/varsel/src/test/java/com/varsel/ElectricityPricesTests/ElectricityPriceDataTests.java
@@ -23,4 +23,43 @@ public class ElectricityPriceDataTests {
         Assertions.assertEquals(expectedTimeStop, data.getTimeStop(), "Expected time stop should be: 21.00");
     }
 
+    @Test
+    @DisplayName("Checks if getter works with negative price values")
+    public void electricityPriceDataTestNegativePriceValues() {
+        // Arrange 
+        double negativePrice = -1.3872;
+        String timeStart = "08.00";
+        String timeStop = "09.00";
+
+        // Act
+        ElectricityPriceData data = new ElectricityPriceData(negativePrice, timeStart, timeStop);
+
+        // Assert
+        Assertions.assertEquals(negativePrice, data.getPrice(), "Expected price should be: -1.3872");
+        Assertions.assertEquals(timeStart, data.getTimeStart(), "Expected time start should be: 08.00");
+        Assertions.assertEquals(timeStop, data.getTimeStop(), "Expected time stop should be: 09.00");
+    }
+
+
+    @Test
+    @DisplayName("Checks if getter works with 'null' values in timeStart and timeStop")
+    public void electricityPriceDataTestNullValuesInTimeParameters() {
+        // Arrange 
+        double price = 21.382;
+        String timeStartNull = null;
+        String timeStopNull = null;
+
+        // Act
+        ElectricityPriceData data = new ElectricityPriceData(price, timeStartNull, timeStopNull);
+
+        // Assert
+        Assertions.assertEquals(price, data.getPrice(), "Expected price should be: 21.382");
+        Assertions.assertEquals(timeStartNull, data.getTimeStart(), "Expected time start should be: null");
+        Assertions.assertEquals(timeStopNull, data.getTimeStop(), "Expected time stop should be: null");
+    }
+
+
+
+
+
 }
