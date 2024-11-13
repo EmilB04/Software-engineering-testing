c34c99db8b915a801c918164bb785e8b1810f8db
Thomsen97 <sebastwt@hiof.no>
2024-11-14 00:05:14 +0100

    Transferred files from java-testing branch to develop with new structure

---
+++ S.E.O.T_Gruppe-9/VarsEL/back-end/varsel/src/main/java/com/varsel/Users/User.java
@@ -0,0 +1,26 @@
+package varsel.login_related;
+
+// Path: varsel/src/main/java/varsel/login_related/User.java
+// Use-case: User.java is a class that represents a user in the system.
+// Attributes: email, and password.
+
+/**
+ * Denne klassen representerer en bruker med e-post og kryptert passord.
+ */
+public class User {
+    private final String email;
+    private final String hashedPassword;
+
+    public User(String email, String hashedPassword) {
+        this.email = email;
+        this.hashedPassword = hashedPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+}
