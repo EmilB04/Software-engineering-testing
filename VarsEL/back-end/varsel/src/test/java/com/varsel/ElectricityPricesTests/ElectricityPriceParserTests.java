244dbc36b0d05809401c40483658dc4bda9c40d9
Andolaus <andolaus@gmail.com>
2024-11-13 10:01:32 +0100

    Laget tester i ElectricityPriceParser for å teste exceptions når det er ingen data å parse, når det er null og når det bare er whitespace

--- VarsEL/back-end/varsel/src/test/java/com/varsel/ElectricityPricesTests/ElectricityPriceParserTests.java
+++ VarsEL/back-end/varsel/src/test/java/com/varsel/ElectricityPricesTests/ElectricityPriceParserTests.java
@@ -18,5 +18,28 @@ public class ElectricityPriceParserTests {
         Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parse(jsonData),
         "Empty JSON-data should throw exception");
     }
+
+    @Test 
+    @DisplayName("Testing to see if ElectricityPriceParser throws exception when there's null JSON-data to parse")
+    public void electricityPriceParserTestNullJsonData() {
+        // Arrange 
+        ElectricityPriceParser parser = new ElectricityPriceParser();
+
+        // Act and assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parse(null),
+        "Null JSON-data should throw exception");
+    }
+
+    @Test 
+    @DisplayName("Testing to see if ElectricityPriceParser throws exception when there's only whitespace when trying to parse")
+    public void electricityPriceParserTestExceptionWhenWhitespace() {
+        // Arrange 
+        ElectricityPriceParser parser = new ElectricityPriceParser();
+        String jsonData = "        ";
+
+        // Act and assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parse(jsonData),
+        "Only whitespace in JSON-data should throw exception");
+    }
     
 }
