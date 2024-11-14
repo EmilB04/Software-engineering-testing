ffa3635f4705ac4406a96970841b4c5bef38f64f
Andolaus <andolaus@gmail.com>
2024-11-14 21:17:03 +0100

    Måtte fikse på HttpHandler da den ikke var skrevet godt nok for testing

--- VarsEL/back-end/varsel/src/main/java/com/varsel/ElectricityPrices/HttpHandler.java
+++ VarsEL/back-end/varsel/src/main/java/com/varsel/ElectricityPrices/HttpHandler.java
@@ -1,10 +1,13 @@
 package com.varsel.ElectricityPrices;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,34 +33,35 @@ public class HttpHandler {
      * @return JSON data as a String
      * @throws IOException if a network error occurs
      */
-    public String getJSONDataFromUrl (String urlInput){
+    public String getJSONDataFromUrl (String urlInput) throws IOException, URISyntaxException { 
 
         StringBuilder data = new StringBuilder(); 
+        HttpURLConnection connection = null;
 
         try {
             URI uri = new URI(urlInput);
             URL url = uri.toURL(); 
 
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection(); 
+            connection = (HttpURLConnection) url.openConnection(); 
             connection.setRequestMethod("GET");
-            logger.info("Koblet til: {}", urlInput);
+            logger.info("Connected to: {}", urlInput);
 
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
             String infoLine;
-
-             while ((infoLine = in.readLine()) != null) {
+            while ((infoLine = in.readLine()) != null) {
                 data.append(infoLine);
             }
 
             in.close();
-            connection.disconnect();
-
             logger.info("Fetched data from site: "); 
-    } catch (Exception e) {
-        logger.error("An error occurred while retrieving data from the site ", e);
-        e.printStackTrace();
-    }
-    return data.toString();
+        } catch (Exception e) {
+            logger.error("An error occurred while retrieving data from the site ", e);
+            throw e;
+         } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
+        return data.toString();
+    }
 }
