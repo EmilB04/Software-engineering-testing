c9b120fe1865051181ef5f1be1d70da8ae39ba53
Thomsen97 <sebastwt@hiof.no>
2024-11-14 16:57:57 +0100

    Updated User class with comments

--- VarsEL/back-end/varsel/src/main/java/com/varsel/Users/User.java
+++ VarsEL/back-end/varsel/src/main/java/com/varsel/Users/User.java
@@ -1,9 +1,5 @@
 package com.varsel.Users;
 
-// Path: varsel/src/main/java/varsel/login_related/User.java
-// Use-case: User.java is a class that represents a user in the system.
-// Attributes: email, and password.
-
 /**
  * Denne klassen representerer en bruker med e-post og kryptert passord.
  */
@@ -11,15 +7,31 @@ public class User {
     private final String email;
     private final String hashedPassword;
 
+    /**
+     * Konstruktør som initialiserer en bruker med e-post og kryptert passord.
+     *
+     * @param email          E-postadresse til brukeren
+     * @param hashedPassword Hashet versjon av brukerens passord
+     */
     public User(String email, String hashedPassword) {
         this.email = email;
         this.hashedPassword = hashedPassword;
     }
 
+    /**
+     * Henter e-postadressen til brukeren.
+     *
+     * @return e-postadressen som en String
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Henter den krypterte versjonen av brukerens passord.
+     *
+     * @return Hashet passord som en String
+     */
     public String getHashedPassword() {
         return hashedPassword;
     }
