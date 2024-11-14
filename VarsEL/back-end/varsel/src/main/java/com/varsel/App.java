af7e38d9a56f5792d301f3fb535a22897efaf624
Thomsen97 <sebastwt@hiof.no>
2024-11-14 01:31:33 +0100

    Oppdatert App.java
    La til en kommentert ut kode som hjelper å vise hvordan lagring og lesing av brukerinformasjon fungerer

--- VarsEL/back-end/varsel/src/main/java/com/varsel/App.java
+++ VarsEL/back-end/varsel/src/main/java/com/varsel/App.java
@@ -9,5 +9,16 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        // Under er kode som tester registrering og innlogging ved hjelp av et enkelt brukergrensesnitt.
+        // Det lager en tekstfil som lagrer input email og passord(kryptert), og leser input fra bruker
+        // dersom man ønsker å logge inn og sjekker om det er likt noe av det som er lagret i filen.
+        /*
+        UserRepository userRepository = new UserRepository(FILE_PATH);
+        UserService userService = new UserService(userRepository);
+        LoginMenu loginMenu = new LoginMenu(userService);
+
+        loginMenu.start();  // Starter menyen
+         */
     }
 }
