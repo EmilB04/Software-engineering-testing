fa8ddf7a433b4c4c3ecea091d981794c5f8221f4
Emil Berglund <emil.berglund@live.no>
2024-11-21 00:17:34 +0100

    Add multiple choices for running

--- VarsEL/back-end/varsel/src/main/java/com/varsel/App.java
+++ VarsEL/back-end/varsel/src/main/java/com/varsel/App.java
@@ -5,6 +5,7 @@ import com.varsel.Users.UserRepository;
 import com.varsel.Users.UserService;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -15,22 +16,55 @@ public class App
     //private static final String FILE_PATH = "users.txt";
 
     public static void main(String[] args ) throws IOException {
-        System.out.println( "Hello World!" );
+        System.out.println( "Velkommen til test-siden" );
 
         /*
-         Under er kode som tester registrering og innlogging ved hjelp av et enkelt brukergrensesnitt.
-         Det lager en tekstfil som lagrer input email og passord(kryptert), og leser input fra bruker
-         dersom man ønsker å logge inn og sjekker om det er likt noe av det som er lagret i filen.
+        Under er kode som tester registrering og innlogging ved hjelp av et enkelt brukergrensesnitt.
+        Det lager en tekstfil som lagrer input email og passord(kryptert), og leser input fra bruker
+        dersom man ønsker å logge inn og sjekker om det er likt noe av det som er lagret i filen.
          */
 
-        /*
-        UserRepository userRepository = new UserRepository(FILE_PATH);
+        UserRepository userRepository = new UserRepository("users.txt");
         UserService userService = new UserService(userRepository);
         LoginMenu loginMenu = new LoginMenu(userService);
 
-        loginMenu.start();  // Starter menyen
-        */
+        try (Scanner scanner = new Scanner(System.in)) {
+            String input;
+            while (true) {
+            System.out.print("\nSkriv ett av alternativene under for å teste ulike funksjoner:\n" +
+                "1. User\n" +
+                "2. Strømpris\n" +
+                "3. GPS\n" +
+                "4. Varsling\n" +
+                "5. Avslutt\n" +
+                "Ditt valg: ");
+            input = scanner.nextLine();
 
-         
+            if ("User".equalsIgnoreCase(input) || "1".equals(input)) {
+                System.out.println("\n");
+                loginMenu.start();  // Starter menyen
+            } 
+            else if ("Strømpris".equalsIgnoreCase(input) || "2".equals(input)) {
+                System.out.println("\n");
+                // Strømprisfunksjon
+            } 
+            else if ("GPS".equalsIgnoreCase(input) || "3".equals(input)) {
+                System.out.println("\n");
+                // GPS-funksjon
+            } 
+            else if ("Varsling".equalsIgnoreCase(input) || "4".equals(input)) { {
+                System.out.println("\n");
+                // Varslingsfunksjon
+            }
+            } 
+            else if ("Avslutt".equalsIgnoreCase(input) || "5".equals(input)) {
+                System.out.println("Avslutter programmet.");
+                break;
+            }
+            else {
+                System.out.println("Ugyldig input. Prøv igjen.");
+                }
+            }
+        }
     }
 }
