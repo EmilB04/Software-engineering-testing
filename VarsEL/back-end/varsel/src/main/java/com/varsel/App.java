5a8a1a62e0c2ed15aa7f5c1d2bd2dba532c53e79
idatol <ida.k.tollaksen@hiof.no>
2024-11-21 21:18:01 +0100

    prøver igjen

--- VarsEL/back-end/varsel/src/main/java/com/varsel/App.java
+++ VarsEL/back-end/varsel/src/main/java/com/varsel/App.java
@@ -3,31 +3,10 @@ package com.varsel;
 import com.varsel.Notifications.FirebaseInitializer;
 import com.varsel.Notifications.PushNotifikasjonSender;
 
-import com.varsel.Users.LoginMenu;
-import com.varsel.Users.UserRepository;
-import com.varsel.Users.UserService;
-
-import java.io.IOException;
-import java.util.Scanner;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    //private static final String FILE_PATH = "users.txt";
-
-    public static void main(String[] args ) throws IOException {
-        System.out.println( "Velkommen til test-siden" );
-
-        /*
-        Under er kode som tester registrering og innlogging ved hjelp av et enkelt brukergrensesnitt.
-        Det lager en tekstfil som lagrer input email og passord(kryptert), og leser input fra bruker
-        dersom man ønsker å logge inn og sjekker om det er likt noe av det som er lagret i filen.
-         */
-
-    
+    public static void main(String[] args) {
+            
         /**
          * Koden under viser hvordan push-notifikasjoner kan sendes til en bruker
          * ved hjelp av Firebase Cloud Messaging (FCM) og en service account.
